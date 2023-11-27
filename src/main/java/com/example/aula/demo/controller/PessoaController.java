09e42643e751a3a94b8be0571263fc7c349adaab
lauragpse <lauragpse446@gmail.com>
2023-11-27 17:28:15 -0300

    Java-Aula-Server-Forms

---
+++ src/main/java/com/example/aula/demo/controller/PessoaController.java
@@ -0,0 +1,38 @@
+package com.example.aula.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+/* 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;*/
+
+import com.example.aula.demo.model.Pessoa;
+
+@Controller
+
+public class PessoaController {
+
+    @GetMapping("/create")
+
+    public String create() {
+        return "create";
+    }
+
+    @PostMapping("/create")
+    /*
+     * public @RequestBody String cadastrar(@RequestParam String nome, String
+     * telefone) {
+     */
+
+    public void create(Pessoa pessoa) {
+        System.out.println("Meu nome é: " + pessoa.getNome());
+        System.out.println("Meu telefone é: " + pessoa.getTelefone());
+    }
+    /*
+     * Pessoa pessoa = new Pessoa();
+     * pessoa.setNome(nome);
+     * pessoa.setTelefone(telefone);
+     */
+
+}
